415518afb434755d994e71e2c842e961666d109b
Ellwyn <ellwynpyon@gmail.com>
2026-08-27 08:21:38 -0700

    Gallery routing

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -1,6 +1,12 @@
-import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Navigate, Routes, Route, useSearchParams } from 'react-router-dom';
 import { Hub } from './pages/Hub';
 import { Gallery } from './pages/Gallery';
+
+function GalleryRoute() {
+  const [searchParams] = useSearchParams();
+
+  return searchParams.has('character') ? <Gallery /> : <Hub />;
+}
 
 function App() {
   return (
@@ -8,8 +14,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Hub />} />
         <Route path="/characters" element={<Navigate to="/" replace />} />
-        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/gallery" element={<GalleryRoute />} />
         <Route path="/gallery-noco" element={<Navigate to="/gallery" replace />} />
+
+        {/* Old bare /gallery WIP, retained for future implementation. */}
+        <Route path="/gallery-lab" element={<Gallery />} />
+
         {/* Future routes */}
         {/* <Route path="/movies" element={<Movies />} /> */}
         {/* <Route path="/series" element={<Series />} /> */}
