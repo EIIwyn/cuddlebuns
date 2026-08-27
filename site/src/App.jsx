2a3ae7af29d8a6e75f380e9b877ad2ddb00978f2
ellwyn <ellwynpyon@gmail.com>
2026-08-27 01:02:09 +0000

    Revert "Reworked Gallery"
    This reverts commit 8a13565e7c2871e82a2ab9c7b14f472fb1afe35a.

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -6,13 +6,13 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/gallery" element={<Hub />} />
+        <Route path="/" element={<Hub />} />
         <Route path="/characters" element={<Navigate to="/" replace />} />
+        <Route path="/gallery" element={<Gallery />} />
         <Route path="/gallery-noco" element={<Navigate to="/gallery" replace />} />
-
-        {/* Old /gallery WIP, retained for later repurposing */}
-        <Route path="/gallery-lab" element={<Gallery />} />
-
+        {/* Future routes */}
+        {/* <Route path="/movies" element={<Movies />} /> */}
+        {/* <Route path="/series" element={<Series />} /> */}
       </Routes>
     </BrowserRouter>
   );
